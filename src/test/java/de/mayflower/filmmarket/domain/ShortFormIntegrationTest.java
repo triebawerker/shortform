6ab66bef65a792e132b3b71f15c5e1c12d6fc197
micha <micha.trieba@mayflower.de>
2012-09-16 21:55:35 +0200

    integration test added

--- src/test/java/de/mayflower/filmmarket/domain/ShortFormIntegrationTest.java
+++ src/test/java/de/mayflower/filmmarket/domain/ShortFormIntegrationTest.java
@@ -1,5 +1,7 @@
 package de.mayflower.filmmarket.domain;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.springframework.roo.addon.test.RooIntegrationTest;
 
@@ -8,5 +10,21 @@ public class ShortFormIntegrationTest {
 
     @Test
     public void testMarkerMethod() {
+    }
+    
+    @Test
+    public void testShortForm()
+    {
+    	ShortForm shortForm = new ShortForm();
+    	shortForm.setLicensee("Lizenznehmer");
+    	shortForm.setLicensor("Lizenzgeber");
+    	shortForm.setLicensefee(50L);
+    	shortForm.persist();
+    	shortForm.flush();
+    	shortForm.clear();
+    	
+    	Assert.assertNotNull(shortForm.getId());
+    	Assert.assertEquals("Lizenznehmer", shortForm.getLicensee());
+    	Assert.assertTrue(50L == shortForm.getLicensefee());
     }
 }
